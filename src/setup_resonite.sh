26821f1ad131bf87c724f6f044d7eac1a65d36b9
PointerOffset <61102129+PointerOffset@users.noreply.github.com>
2024-08-07 20:08:11 -0700

    Updated for dotnet8 as main headless

--- src/setup_resonite.sh
+++ src/setup_resonite.sh
@@ -6,10 +6,10 @@ bash "${STEAMCMDDIR}/steamcmd.sh" \
 	+app_license_request ${STEAMAPPID} \
 	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
 	+quit
-find ${STEAMAPPDIR}/Headless/net8.0/Data/Assets -type f -atime +7 -delete
-find ${STEAMAPPDIR}/Headless/net8.0/Data/Cache -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/Data/Assets -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
-mkdir -p Headless/Migrations Headless/net8.0/Migrations
+mkdir -p Headless/Migrations Headless/Migrations
 
 # If Mods flag is enabled, do the setup for mods.
 if [ $ALLOWMODS -eq 1 ]; then
@@ -17,15 +17,15 @@ if [ $ALLOWMODS -eq 1 ]; then
 	chown -R resonite:resonite /RML
 
 	# Create required directories
-	mkdir -p Headless/net8.0/rml_mods \
-			Headless/net8.0/rml_libs \
-			Headless/net8.0/rml_config \
-			Headless/net8.0/Libraries
+	mkdir -p Headless/rml_mods \
+			Headless/rml_libs \
+			Headless/rml_config \
+			Headless/Libraries
 
 	# copy any mods and associated files from our mounted volume
-	cp -a /RML/rml_mods/. ${STEAMAPPDIR}/Headless/net8.0/rml_mods/
-	cp -a /RML/rml_libs/. ${STEAMAPPDIR}/Headless/net8.0/rml_libs/
-	cp -a /RML/rml_config/. ${STEAMAPPDIR}/Headless/net8.0/rml_config/
+	cp -a /RML/rml_mods/. ${STEAMAPPDIR}/Headless/rml_mods/
+	cp -a /RML/rml_libs/. ${STEAMAPPDIR}/Headless/rml_libs/
+	cp -a /RML/rml_config/. ${STEAMAPPDIR}/Headless/rml_config/
 
 	# From: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8?permalink_comment_id=5097031#gistcomment-5097031
 	# Fetch the latest release version number for ResoniteModLoader
@@ -37,8 +37,8 @@ if [ $ALLOWMODS -eq 1 ]; then
 	rmldll_url="https://github.com/resonite-modding-group/ResoniteModLoader/releases/download/${latest_version}/ResoniteModLoader.dll"
 
 	# Download the required DLLs
-	curl -L -o "Headless/net8.0/rml_libs/0Harmony-Net8.dll" "$harmonydll_url"
-	curl -L -o "Headless/net8.0/Libraries/ResoniteModLoader.dll" "$rmldll_url"
+	curl -L -o "Headless/rml_libs/0Harmony-Net8.dll" "$harmonydll_url"
+	curl -L -o "Headless/Libraries/ResoniteModLoader.dll" "$rmldll_url"
 fi
 
 exec $*
